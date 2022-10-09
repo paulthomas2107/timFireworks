f2b496710bf80dccc9d55cc2875b309fc931218c
Paul Thomas <paulthomas2107@gmail.com>
2022-10-09 09:37:44 +0100

    Initial

---
+++ tutorial.py
@@ -0,0 +1,40 @@
+import pygame
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+
+win = pygame.display.set_mode(size=(WIDTH, HEIGHT))
+pygame.display.set_caption('Fireworks !')
+
+FPS = 60
+
+colors = [
+    (255, 0, 0),
+    (0, 255, 0),
+    (0, 0, 255),
+    (0, 255, 255),
+    (255, 165, 0),
+    (255, 255, 255),
+    (230, 230, 250),
+    (255, 192, 203)
+]
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+    quit()
+
+
+if __name__ == '__main__':
+    main()
